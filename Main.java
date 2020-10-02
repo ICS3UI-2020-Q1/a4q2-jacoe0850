803e6a23abfac3618d11ac384666e7a25e0d54ba
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-02 23:18:49 +0000

    Finished a4q2

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,14 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+   Scanner input = new Scanner(System.in);
+   //declare variable
+   int num;
+   do{
+     System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+     num = input.nextInt();
+   }while(num > 0);
+   System.out.println("All done!");
     
   }
 }
